d4052f9d07324ecfb11936368ae042e5ec9535c1
kayukin <kayukin@gmail.com>
2015-10-03 14:22:03 +0600

    -

---
+++ src/main/java/net/thumbtack/kayukin/trainee/Trainee.java
@@ -0,0 +1,13 @@
+package net.thumbtack.kayukin.trainee;
+
+/**
+ * Hello world!
+ *
+ */
+public class Trainee
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/net/thumbtack/kayukin/trainee/TraineeTest.java
@@ -0,0 +1,10 @@
+package net.thumbtack.kayukin.trainee;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Александр on 03.10.2015.
+ */
+public class TraineeTest {
+
+}
