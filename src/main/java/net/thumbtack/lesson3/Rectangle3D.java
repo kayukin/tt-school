6a1e89363e2a3036b0cf243dd8d3dc7e08220ae9
kayukin <kayukin@gmail.com>
2015-10-18 16:52:22 +0700

    Lesson 3

---
+++ src/main/java/net/thumbtack/lesson3/Rectangle3D.java
@@ -0,0 +1,7 @@
+package net.thumbtack.lesson3;
+
+/**
+ * Created by kayukin on 18.10.15.
+ */
+public class Rectangle3D {
+}
---
+++ src/test/java/net/thumbtack/lesson3/Rectangle3DTest.java
@@ -0,0 +1,61 @@
+package net.thumbtack.lesson3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kayukin on 18.10.15.
+ */
+public class Rectangle3DTest {
+
+    @Test
+    public void testGetZ() throws Exception {
+
+    }
+
+    @Test
+    public void testSetZ() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLength() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLength() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testArea() throws Exception {
+
+    }
+
+    @Test
+    public void testVolume() throws Exception {
+
+    }
+
+    @Test
+    public void testIsPointIn() throws Exception {
+
+    }
+
+    @Test
+    public void testIsPointIn1() throws Exception {
+
+    }
+}
